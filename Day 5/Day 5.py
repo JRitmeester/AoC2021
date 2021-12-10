97f6a71cd84a6dc3ddca33758aeee82ec9901a1c
JRitmeester <jeroenritmeester1407@gmail.com>
2021-12-10 17:27:55 +0100

    Actually completed Day 5

--- Day 5/Day 5.py
+++ Day 5/Day 5.py
@@ -4,29 +4,55 @@ import matplotlib.pyplot as plt
 from pprint import pprint
 
 
+def create_map(lines: list, include_diagonal: bool):
+    grid = np.zeros((2000, 2000))
+
+    max_x = 0
+    max_y = 0
+
+    for line in lines:
+        start = np.array(line[0])
+        end = np.array(line[1])
+        delta = np.array([end[0] - start[0], end[1] - start[1]])
+        length = np.max(np.abs(delta))
+        dir = delta / length
+
+
+        if not include_diagonal:
+            if np.sum(np.abs(dir)) != 1:
+                continue
+
+        for step in range(length+1):
+            current = (start + step * dir).astype(np.int)
+            grid[current[1], current[0]] += 1
+            max_x = max(max_x, current[0])
+            max_y = max(max_y, current[1])
+
+    grid = grid[:max_y, :max_x]
+
+    return grid
+
+
+
 def main():
     input_raw = (Path.cwd() / "input.txt").read_text()
     input_split = input_raw.split('\n')[:-1]
-    lines = [[tuple(int(x) for x in p.split(',')) for p in line.split(' -> ')] for line in input_split]
-    dirs = [(line[0][0] - line[1][0] // max(abs(line[0][0] - line[1][0]), abs(line[1][0] - line[1][1])),
-             line[1][0] - line[1][1] // max(abs(line[0][0] - line[1][0]), abs(line[1][0] - line[1][1])))
-            for line in lines]
+    lines = [[tuple(int(el) for el in point.split(',')) for point in line.split(' -> ')] for line in input_split]
 
-    grid = np.zeros((988, 988))
-
-    print(dirs)
-    for line, dir in zip(lines, dirs):
-        if sum(dir) == 1:
-            current = line[0]
-            # print(current)
-            while current != line[1]:
-                grid[current[0], current[1]] += 1
-                current[0] += dir[0]
-                current[1] += dir[1]
-                print(current)
-        # break
+    # Part 1
+    grid = create_map(lines, include_diagonal=False)
     plt.imshow(grid)
+    print("Answer part 1:", np.count_nonzero(grid > 1))
+
+    # Part 2
+    grid = create_map(lines, include_diagonal=True)
+    plt.imshow(grid)
+    print("Answer part 2:", np.count_nonzero(grid > 1))
+
     plt.show()
+
+
+
 
 
 
