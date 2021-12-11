b33c542ae39af0efb6f597b25e0bfd17630f103f
JRitmeester <jeroenritmeester1407@gmail.com>
2021-12-11 16:55:28 +0100

    Completed Day 11

--- Day 11/Day 11.py
+++ Day 11/Day 11.py
@@ -5,11 +5,19 @@ from pprint import pprint
 import imageio
 
 # Is it octopi, octopuses or something else? In this house, it is octos.
+# There is a GIF export in here, but I'm not entirely sure the result is correct.
+# The answers are correct, though.
 
 
 def create_gif(image_folder: Path, name: str):
     images = [imageio.imread(file) for file in image_folder.glob('*.png')]
     imageio.mimsave(image_folder/name, images)
+
+
+def save_image(image_arr, folder, name):
+    plt.imshow(image_arr)
+    plt.savefig(folder/name)
+    plt.close()
 
 
 def step(octos):
@@ -45,35 +53,62 @@ def step(octos):
     return octos, flashes
 
 
+def get_octos(input_raw):
+    return np.array([[int(energy) for energy in line] for line in input_raw.split('\n')])
+
+
 def main():
     input_raw = (Path.cwd() / "input.txt").read_text()
-    lines = input_raw.split('\n')
+    octos = get_octos(input_raw)
 
-    octos = np.array([[int(energy) for energy in line] for line in lines])
-
+    # Part 1
     # Set up extra stuff to export the images to make a GIF from it.
     # It looks pretty cool! (Takes a little while though).
     make_gif = False
     folder = Path.cwd() / 'images_part_1'
     if make_gif:
         folder.mkdir(exist_ok=True)
-        plt.imshow(octos)
-        plt.savefig(folder/'0.png')
+        save_image(octos, folder, '0.png')
 
+    # Start simulating the flashes for 100 steps.
     total = 0
     for i in range(100):
         octos, flashes = step(octos)
-
         if make_gif:
-            plt.imshow(octos)
-            plt.savefig(folder/f'{i+1}.png')
-            plt.close()
-
+            save_image(octos, folder, f'{i+1}.png')
         total += flashes
 
     print("Answer part 1:", total)
+
     if make_gif:
         create_gif(folder, 'part1.gif')
+
+    # Part 2
+
+    # Reset the octos (you could continue from step 100 but that just
+    # get confusing to me...
+    octos = get_octos(input_raw)
+
+    make_gif = True
+    folder = Path.cwd() / 'images_part_2'
+    if make_gif:
+        folder.mkdir(exist_ok=True)
+        save_image(octos, folder, '0.png')
+
+    steps = 0
+    while True:
+        steps += 1
+        octos, _ = step(octos)
+        
+        if make_gif:
+            save_image(octos, folder, f'{steps}.png')
+            
+        if np.sum(octos) == 0:
+            break
+
+    print("Answer part 2:", steps)
+    create_gif(folder, 'part2.gif')
+
 
 
 if __name__ == "__main__":
