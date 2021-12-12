edaed795d964bb80a817fd2f32b68ff5923cdb9e
JRitmeester <jeroenritmeester1407@gmail.com>
2021-12-12 23:48:14 +0100

    Skipped Day 12

---
+++ Day 12/Day 12.py
@@ -0,0 +1,13 @@
+import numpy as np
+from pathlib import Path
+import matplotlib.pyplot as plt
+from pprint import pprint
+
+
+def main():
+    # Yeah... I don't do pathfinding.
+    pass
+
+
+if __name__ == "__main__":
+    main()
